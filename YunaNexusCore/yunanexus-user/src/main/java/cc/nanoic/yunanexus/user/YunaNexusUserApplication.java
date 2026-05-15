fa8973cfcd9fb36a727ff186a775e6390e03362f
Nanoic39 <2026256242@qq.com>
2026-05-15 22:57:59 +0800

    feat(core-user/core-auth): 修复无法正确抛出异常的问题（前端测试相同账号注册时发现无法收到正确的业务报错响应，于是发现了这个问题，果然还是很需要测试人员的啊😭）

--- YunaNexusCore/yunanexus-user/src/main/java/cc/nanoic/yunanexus/user/YunaNexusUserApplication.java
+++ YunaNexusCore/yunanexus-user/src/main/java/cc/nanoic/yunanexus/user/YunaNexusUserApplication.java
@@ -1,15 +1,18 @@
 package cc.nanoic.yunanexus.user;
 
+import cc.nanoic.yunanexus.common.web.common.GlobalExceptionHandler;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "cc.nanoic.yunanexus.user.client")
 @MapperScan("cc.nanoic.yunanexus.user.mapper")
 @EnableScheduling
+@Import(GlobalExceptionHandler.class)
 public class YunaNexusUserApplication {
     public static void main(String[] args) {
         SpringApplication.run(YunaNexusUserApplication.class, args);
